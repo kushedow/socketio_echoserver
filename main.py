2870a4bc5bf2745ea4255204ab4a174e21e4fb29
Gleb Kushedow <kushedow@gmail.com>
2023-07-28 03:38:50 +0400

    add /filetree event and filetree variable

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import eventlet
 import socketio
 
 sio = socketio.Server()
-
 app = socketio.WSGIApp(sio)
+
+filetree = [
+
+    {"type": "folder", "path": "/"},
+    {"type": "file", "path": "/run.py", "content": "hello_world()"},
+
+    {"type": "folder", "path": "/data"},
+    {"type": "file", "path": "/data/one.json", "content": "{}"},
+    {"type": "file", "path": "/data/two.json", "content": "{}"},
+
+    {"type": "folder", "path": "/src"},
+    {"type": "file", "path": "/src/app.py",  "content": "app.run()"},
+
+]
 
 
 @sio.event
@@ -14,6 +27,11 @@ def connect(sid, data):
 def broadcast_message(sid, data):
     message = data.get("message")
     sio.emit('message', data={"message": message, "sid": sid})
+
+@sio.on("filetree")
+def broadcast_filetree(sid, data):
+    sio.emit('message', to=sid, data={"filetree": filetree})
+
 
 @sio.event
 def disconnect(sid):
